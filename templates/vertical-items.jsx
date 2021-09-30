2d50a0f8f163246d52e52f3c09115471ac08e144
Ignacio Cinalli <nachocinalli@gmail.com>
2021-09-30 16:43:12 -0300

    Update template

--- templates/vertical-items.jsx
+++ templates/vertical-items.jsx
@@ -8,16 +8,16 @@ export default function VerticalItems({
   ...props
 }) {
   return (
-    <div className="component__inner verticalitems__inner">
+    <div className='component__inner verticalitems__inner'>
       <templates.header {...props} />
-      <div className="component__widget verticalitems__widget">
+      <div className='component__widget verticalitems__widget'>
         {top && (
           <div className='verticalitems__top u-clearfix'>
             <div className='verticalitems__top-inner'>{html(compile(top))}
             </div>
           </div>
         )}
-        <div className='verticalitems__container'>
+        <div className='verticalitems__container' role='list'>
           {_items.map(({ title, body, _icon, _graphic, _isVisited, _classes }, _index) => (
             <div
               className={classes([
@@ -47,17 +47,17 @@ export default function VerticalItems({
               <div className={classes(['verticalitems__item__content', _isVisited ? 'anim' : 'is-hidden'])}>
 
                 {title && (
-                  <div className="verticalitems__item__content-title">
+                  <div className='verticalitems__item__content-title'>
                     {html(compile(title))}
                   </div>
                 )}
                 {body && (
-                  <div className="verticalitems__item__content-body">
+                  <div className='verticalitems__item__content-body'>
                     {html(compile(body))}
                   </div>
                 )}
                 {_graphic.src && (
-                  <div className="verticalitems__item__content-graphic">
+                  <div className='verticalitems__item__content-graphic'>
                     <img src={_graphic.src} alt={_graphic.alt} />
                   </div>
                 )}
